3d672eccf3f9de03fc02e11ce27c3e57e0340982
Kenysson Cavalcante <kacs1@aluno.ifal.edu.br>
2021-11-29 22:23:51 -0300

    Revert "Como criar seu próprio Hook"
    This reverts commit 71887051f3b081f98bb870e1e579f26d200b222d.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
-import Rotas from "./Rotas";
+import logo from './logo.svg';
+import './App.css';
 
-function App(){
-  return(
-    <div>
-      <Rotas />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
